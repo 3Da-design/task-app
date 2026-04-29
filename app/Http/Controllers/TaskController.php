e3ae905f9f863c575732307dff1cfae24c89ba45
Y.S <184586909+Yuto-design@users.noreply.github.com>
2026-04-29 15:58:15 +0900

    Udate task-app

---
+++ app/Http/Controllers/TaskController.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Task;
+use Illuminate\Http\Request;
+
+class TaskController extends Controller
+{
+    public function index()
+    {
+        return Task::all();
+    }
+
+    public function store(Request $request)
+    {
+        $task = Task::create([
+            'title' => $request->title
+        ]);
+
+        return response()->json($task, 201);
+    }
+}
